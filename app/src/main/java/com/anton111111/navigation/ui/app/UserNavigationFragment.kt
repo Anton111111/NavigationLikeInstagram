86b33ba178c32cef37987631610234c751acf907
anton <anton.potekhin@orbitsoft.com>
2021-01-31 00:41:12 +0300

    Setup bottom view on when started to fix problem with back from another graph. BottomNavigationView.selectedItemId wrong on onViewCreated

--- app/src/main/java/com/anton111111/navigation/ui/app/UserNavigationFragment.kt
+++ app/src/main/java/com/anton111111/navigation/ui/app/UserNavigationFragment.kt
@@ -3,7 +3,7 @@ package com.anton111111.navigation.ui.app
 import android.os.Bundle
 import android.view.View
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.lifecycleScope
 import androidx.navigation.NavController
 import by.kirich1409.viewbindingdelegate.viewBinding
 import com.anton111111.navigation.R
@@ -14,33 +14,37 @@ import kotlinx.coroutines.flow.StateFlow
 class UserNavigationFragment : Fragment(R.layout.user_navigation_fragment) {
 
     private val viewBinding: UserNavigationFragmentBinding by viewBinding()
-    private lateinit var currentNavController: StateFlow<NavController?>
+    private var currentNavController: StateFlow<NavController?>? = null
+
 
     override fun onViewStateRestored(savedInstanceState: Bundle?) {
         super.onViewStateRestored(savedInstanceState)
-        if (savedInstanceState != null)
-            setupBottomNavigationBar()
-    }
-
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        if (savedInstanceState == null) {
+        if (savedInstanceState != null) {
             setupBottomNavigationBar()
         }
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        if (savedInstanceState == null)
+            setupBottomNavigationBar()
+    }
+
     private fun setupBottomNavigationBar() {
-        currentNavController = viewBinding.bottomNav.setupWithNavController(
-            navGraphIds = listOf(
-                R.navigation.home_navigation,
-                R.navigation.search_navigation,
-                R.navigation.favorites_navigation,
-                R.navigation.profile_navigation
-            ),
-            fragmentManager = childFragmentManager,
-            containerId = viewBinding.navHost.id,
-            intent = requireActivity().intent
-        )
+        viewLifecycleOwner.lifecycleScope.launchWhenStarted {
+            currentNavController = viewBinding.bottomNav.setupWithNavController(
+                navGraphIds = listOf(
+                    R.navigation.home_navigation,
+                    R.navigation.search_navigation,
+                    R.navigation.favorites_navigation,
+                    R.navigation.profile_navigation
+                ),
+                fragmentManager = childFragmentManager,
+                containerId = viewBinding.navHost.id,
+                intent = requireActivity().intent
+            )
+        }
+
     }
 
 
